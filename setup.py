979197994c635f45f203c8207bb2e5dc130642e6
Wanchao Zhang <wonderbeyond@gmail.com>
2018-11-14 14:29:32 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ import re
 from setuptools import setup, find_packages
 
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
 def get_version():
     here = os.path.abspath(os.path.dirname(__file__))
     with open(os.path.join(here, 'ustorage', '__init__.py')) as f:
@@ -14,7 +18,8 @@ setup(
     name='ustorage',
     version=get_version(),
     description='Unified Storage Interface for Python.',
-    license='Apache License 2.0',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/wonderbeyond/ustorage',
     author='wonderbeyond',
     author_email='wonderbeyond@gmail.com',
@@ -25,5 +30,11 @@ setup(
     extras_require={
         's3': 'boto3==1.*',
     },
-    zip_safe=False,
+    zip_safe=True,
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
 )
